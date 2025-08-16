74069a96edc807e916d6d5423b128ec696a1f366
Sydney Brookstein <sydneybrookstein@Sydneys-MacBook-Pro.local>
2025-08-15 17:10:02 -0700

    attempt to use netlify for contact form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,17 @@
 import '../styles/ContactPage.css';
+import { useState } from 'react';
 
 const ContactPage = () => {
   return (
     <div className="contact-container">
-      <p>We would love to hear from you! Please leave your message below
-         and we will get back to you as soon as possible.
-      </p>
+      <form name = "contact v1"
+            method = "post" data-netlify="true"
+            onSubmit="submit">
+            <input type="hidden" name="form-name" value="contact v1"/>
+            <label>Enter your name:</label>
+        <input type="text" name="name"/>
+            <button type="submit">Submit The Results</button>
+      </form>
     </div>
   );
 };
